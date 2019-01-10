AOSDP/android_vendor_aosdp

vendor: Add modular backuptool.sh
Tips & Tricks
=============
* 50-base.sh contains only a reference implementation.  You may customize the methods however you wish.
  For example, 20-foobar.sh pre-backup can use a loop with conditionals to generate a dynamic backup list in
  /tmp/foobar_file_list which is later printed by list_files() so the backup method will act on those files.
* Optional methods pre-backup, post-backup, pre-restore, or post-restore may be defined for special purposes.
* Inject new files into /tmp/addon.d/ prior to backuptool.sh backup if you want to act during the current system upgrade.
* Delete files from /tmp/addon.d/ during post-restore if you want to permanently remove files from /system/addon.d/
  Addons may use this approach to run a script only once.
* Scripts run in sort -n order.  Prefix with numbers 00 through 99 if want to run in a particular order.
* You can have two separate scripts, implementing only backup in one, and only restore in the other with a different
  number prefix of each.  This allows even greater control the backup/restore order even further.
* You could use pre-backup to generate a one-time use backup script in /tmp/addon.d/ that deletes itself in
  post-restore.

Change-Id: I36631f4dde3cb81c1228bb6cc4d38a477897a207
Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

config/common.mk
@@ -8,6 +8,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/aosdp/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/aosdp/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/aosdp/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \

XperiaOS/platform_vendor_xperia

config/common.mk
@@ -8,6 +8,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=$(PRODUCT_GMS_CLIENTID_BASE)
 endif
 
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/xperia/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/xperia/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/xperia/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     dalvik.vm.debug.alloc=0 \