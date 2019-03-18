AOSDP/android_vendor_aosdp

vendor: Import APNs from LineageOS
<!-- Credits to LineageOS team -->

Reference:

prebuilt/common/Android.mk -- Left at commit https://github.com/LineageOS/android_vendor_lineage/commit/0d2fa55f2f88e73018a749a3fe34f33ec365fbbc
prebuilt/common/etc/apns-conf.xml -- Left at commit https://github.com/LineageOS/android_vendor_lineage/commit/42252b823b187e8c363723108e16f288fd3fc1be
prebuilt/common/etc/sensitive_pn.xml -- Left at commit https://github.com/LineageOS/android_vendor_lineage/commit/ab6ec3af078711d2b69def41dc1399aace198da4
tools/custom_apns.py -- Left at commit https://github.com/LineageOS/android_vendor_lineage/commit/0d2fa55f2f88e73018a749a3fe34f33ec365fbbc

Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

config/common_full_phone.mk
@@ -1,14 +1,14 @@
 # Inherit common stuff
 $(call inherit-product, vendor/aosdp/config/common.mk)
+
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/aosdp/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
+
 # World APN list
-PRODUCT_COPY_FILES += \
-    vendor/aosdp/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/aosdp/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    vendor/aosdp/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk

prebuilt/common/Android.mk
@@ -1,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+# Copies the APN list file into system/etc for the product as apns-conf.xml.
+# In the case where $(CUSTOM_APNS_FILE) is defined, the content of $(CUSTOM_APNS_FILE)
+# is added or replaced to the $(DEFAULT_APNS_FILE).
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := apns-conf.xml
+LOCAL_MODULE_CLASS := ETC
+
+DEFAULT_APNS_FILE := vendor/aosdp/prebuilt/common/etc/apns-conf.xml
+
+ifdef CUSTOM_APNS_FILE
+CUSTOM_APNS_SCRIPT := vendor/aosdp/prebuilt/common/tools/custom_apns.py
+FINAL_APNS_FILE := $(local-generated-sources-dir)/apns-conf.xml
+
+$(FINAL_APNS_FILE): PRIVATE_SCRIPT := $(CUSTOM_APNS_SCRIPT)
+$(FINAL_APNS_FILE): PRIVATE_CUSTOM_APNS_FILE := $(CUSTOM_APNS_FILE)
+$(FINAL_APNS_FILE): $(CUSTOM_APNS_SCRIPT) $(DEFAULT_APNS_FILE)
+	rm -f $@
+	python $(PRIVATE_SCRIPT) $@ $(PRIVATE_CUSTOM_APNS_FILE)
+else
+FINAL_APNS_FILE := $(DEFAULT_APNS_FILE)
+endif
+
+LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
+
+include $(BUILD_PREBUILT)

XperiaOS/platform_vendor_xperia

config/common_full_phone.mk
@@ -1,14 +1,14 @@
 # Inherit common stuff
 $(call inherit-product, vendor/xperia/config/common.mk)
+
+# Sensitive Phone Numbers list
+PRODUCT_COPY_FILES += \
+    vendor/xperia/prebuilt/common/etc/sensitive_pn.xml:system/etc/sensitive_pn.xml
+
 # World APN list
-PRODUCT_COPY_FILES += \
-    vendor/xperia/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
-# World SPN overrides list
-PRODUCT_COPY_FILES += \
-    vendor/xperia/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
-# Selective SPN list for operator number who has the problem.
-PRODUCT_COPY_FILES += \
-    vendor/xperia/prebuilt/common/etc/selective-spn-conf.xml:system/etc/selective-spn-conf.xml
+PRODUCT_PACKAGES += \
+    apns-conf.xml
+
 # SIM Toolkit
 PRODUCT_PACKAGES += \
     Stk

prebuilt/common/Android.mk
@@ -1,0 +1,28 @@
+LOCAL_PATH := $(call my-dir)
+
+# Copies the APN list file into system/etc for the product as apns-conf.xml.
+# In the case where $(CUSTOM_APNS_FILE) is defined, the content of $(CUSTOM_APNS_FILE)
+# is added or replaced to the $(DEFAULT_APNS_FILE).
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := apns-conf.xml
+LOCAL_MODULE_CLASS := ETC
+
+DEFAULT_APNS_FILE := vendor/xperia/prebuilt/common/etc/apns-conf.xml
+
+ifdef CUSTOM_APNS_FILE
+CUSTOM_APNS_SCRIPT := vendor/xperia/prebuilt/common/tools/custom_apns.py
+FINAL_APNS_FILE := $(local-generated-sources-dir)/apns-conf.xml
+
+$(FINAL_APNS_FILE): PRIVATE_SCRIPT := $(CUSTOM_APNS_SCRIPT)
+$(FINAL_APNS_FILE): PRIVATE_CUSTOM_APNS_FILE := $(CUSTOM_APNS_FILE)
+$(FINAL_APNS_FILE): $(CUSTOM_APNS_SCRIPT) $(DEFAULT_APNS_FILE)
+	rm -f $@
+	python $(PRIVATE_SCRIPT) $@ $(PRIVATE_CUSTOM_APNS_FILE)
+else
+FINAL_APNS_FILE := $(DEFAULT_APNS_FILE)
+endif
+
+LOCAL_PREBUILT_MODULE_FILE := $(FINAL_APNS_FILE)
+
+include $(BUILD_PREBUILT)