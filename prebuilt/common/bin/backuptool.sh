AOSDP/android_vendor_aosdp

vendor: Fix backuptool for all
  * A/B backuptool should run now
  * A backuptool should actually check if we're on AOSDP Pie now
  * Remove a trailing whitespace

Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
Change-Id: I5434992ac63dc41cf6a18433fc01926c4b11bde5

prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,9 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=9.0
+export V=9
+
+export ADDOND_VERSION=1
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
@@ -34,10 +36,10 @@ check_prereq() {
 if [ ! -r /system/build.prop ]; then
     return 0
 fi
-# if [ ! grep -q "^ro.aosdp.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
-#   echo "Not backing up files from incompatible version: $V"
-#   return 0
-# fi
+ if [ ! grep -q "^ro.aosdp.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
+   echo "Not backing up files from incompatible version: $V"
+   return 0
+ fi
 return 1
 }
 

prebuilt/common/bin/backuptool_ab.sh
@@ -5,7 +5,7 @@
 
 export S=/system
 export C=/postinstall/tmp/backupdir
-export V=15.1
+export V=9
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 mkdir -p /postinstall/tmp/
@@ -36,7 +36,7 @@ if [ ! -r /system/build.prop ]; then
     return 0
 fi
 
-grep -q "^ro.lineage.version=$V.*" /system/etc/prop.default /system/build.prop && return 1
+grep -q "^ro.aosdp.version=$V.*" /system/etc/prop.default /system/build.prop && return 1
 
 echo "Not backing up files from incompatible version: $V"
 return 0

XperiaOS/platform_vendor_xperia

prebuilt/common/bin/backuptool.sh
@@ -5,7 +5,9 @@
 
 export C=/tmp/backupdir
 export S=/system
-export V=9.0
+export V=9
+
+export ADDOND_VERSION=1
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 cp -f /tmp/install/bin/backuptool.functions /tmp
@@ -34,10 +36,10 @@ check_prereq() {
 if [ ! -r /system/build.prop ]; then
     return 0
 fi
-# if [ ! grep -q "^ro.xperia.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
-#   echo "Not backing up files from incompatible version: $V"
-#   return 0
-# fi
+ if [ ! grep -q "^ro.xperia.version=$V.*" /system/etc/prop.default /system/build.prop ]; then
+   echo "Not backing up files from incompatible version: $V"
+   return 0
+ fi
 return 1
 }
 

prebuilt/common/bin/backuptool_ab.sh
@@ -5,7 +5,7 @@
 
 export S=/system
 export C=/postinstall/tmp/backupdir
-export V=15.1
+export V=9
 
 # Scripts in /system/addon.d expect to find backuptool.functions in /tmp
 mkdir -p /postinstall/tmp/
@@ -36,7 +36,7 @@ if [ ! -r /system/build.prop ]; then
     return 0
 fi
 
-grep -q "^ro.lineage.version=$V.*" /system/etc/prop.default /system/build.prop && return 1
+grep -q "^ro.xperia.version=$V.*" /system/etc/prop.default /system/build.prop && return 1
 
 echo "Not backing up files from incompatible version: $V"
 return 0